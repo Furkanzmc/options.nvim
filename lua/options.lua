6497c61d67dc98188ebd13c11695f4f5e004f9ef
Furkan Uzumcu <furkanuzumcu@gmail.com>
2021-10-31 17:19:40 -0400

    Format code with stylua

--- lua/options.lua
+++ lua/options.lua
@@ -3,8 +3,8 @@
 local vim = vim
 local cmd = vim.cmd
 local fn = vim.fn
-local log = require "futils.log"
-local typing = require "futils.typing"
+local log = require("futils.log")
+local typing = require("futils.typing")
 local M = {}
 
 local s_registered_options = {}
@@ -15,17 +15,23 @@ local s_variable_sync_initialized = false
 local function init_variable_sync()
     assert(s_variable_sync_initialized == false)
 
-    cmd [[augroup options_nvim_global_var_watch]]
-    cmd [[autocmd!]]
-    cmd [[autocmd CmdlineEnter * if v:event.cmdtype == ":" | call luaeval('require"options".check_for_global_var_change()') | endif]]
-    cmd [[augroup END]]
+    cmd([[augroup options_nvim_global_var_watch]])
+    cmd([[autocmd!]])
+    cmd([[autocmd CmdlineEnter * if v:event.cmdtype == ":" | call luaeval('require"options".check_for_global_var_change()') | endif]])
+    cmd([[augroup END]])
 end
 
-local function set_target_variable(variable, value) vim.g[variable] = value end
+local function set_target_variable(variable, value)
+    vim.g[variable] = value
+end
 
-local function is_target_variable_set(variable) return vim.g[variable] ~= nil end
+local function is_target_variable_set(variable)
+    return vim.g[variable] ~= nil
+end
 
-local function get_target_variable_value(variable) return vim.g[variable] end
+local function get_target_variable_value(variable)
+    return vim.g[variable]
+end
 
 local function is_option_registered(name)
     return s_registered_options[name] ~= nil
@@ -44,13 +50,19 @@ local function get_buffer_option(name, bufnr)
     assert(bufnr ~= nil, "bufnr has to be valid.")
 
     local current_option = s_current_options[name]
-    if current_option == nil then return nil end
+    if current_option == nil then
+        return nil
+    end
 
     local buffers = current_option.buffers
-    if buffers == nil then return nil end
+    if buffers == nil then
+        return nil
+    end
 
     for _, option in ipairs(buffers) do
-        if option.bufnr == bufnr then return option end
+        if option.bufnr == bufnr then
+            return option
+        end
     end
 
     return nil
@@ -59,11 +71,12 @@ end
 local function echo_option(name, bufnr)
     local option_info = get_option_info(name)
     local value = M.get_option_value(name, bufnr)
-    if type(value) == "table" then value = table.concat(value, ",") end
+    if type(value) == "table" then
+        value = table.concat(value, ",")
+    end
 
     if option_info.buffer_local == true then
-        log.info(option_info.source .. "-buflocal",
-                 name .. "=" .. tostring(value))
+        log.info(option_info.source .. "-buflocal", name .. "=" .. tostring(value))
     else
         log.info(option_info.source, name .. "=" .. tostring(value))
     end
@@ -96,9 +109,14 @@ local function echo_options(bufnr)
                 val_str = tostring(value)
             end
 
-            local message =
-                string.rep(" ", 2) .. key .. "=" .. val_str .. " " ..
-                    "[source: " .. option_info.source .. "]"
+            local message = string.rep(" ", 2)
+                .. key
+                .. "="
+                .. val_str
+                .. " "
+                .. "[source: "
+                .. option_info.source
+                .. "]"
 
             table.insert(processed, key)
             if option_info.buffer_local then
@@ -127,9 +145,14 @@ local function echo_options(bufnr)
                     val_str = tostring(info.default)
                 end
 
-                local message = string.rep(" ", 2) .. key .. "=" .. val_str ..
-                                    " " .. "[source: " .. option_info.source ..
-                                    "]"
+                local message = string.rep(" ", 2)
+                    .. key
+                    .. "="
+                    .. val_str
+                    .. " "
+                    .. "[source: "
+                    .. option_info.source
+                    .. "]"
 
                 table.insert(processed, key)
                 if option_info.buffer_local then
@@ -142,29 +165,33 @@ local function echo_options(bufnr)
     end
 
     if #global_options > 0 then
-        log.info("global-options",
-                 "--\\n" .. table.concat(global_options, "\\n"))
+        log.info("global-options", "--\\n" .. table.concat(global_options, "\\n"))
     end
 
     if #buffer_options > 0 then
-        log.info("buffer-options",
-                 "--\\n" .. table.concat(buffer_options, "\\n"))
+        log.info("buffer-options", "--\\n" .. table.concat(buffer_options, "\\n"))
     end
 end
 
 local function execute_callbacks(option_name)
-    if s_callbacks[option_name] == nil then return end
+    if s_callbacks[option_name] == nil then
+        return
+    end
 
-    for _, func in ipairs(s_callbacks[option_name]) do func() end
+    for _, func in ipairs(s_callbacks[option_name]) do
+        func()
+    end
 end
 
 local function split_option_str(option_str)
     local cmps = string.split(option_str, "=")
     local name = cmps[1]
     local value = nil
-    if #cmps == 2 then value = cmps[2] end
+    if #cmps == 2 then
+        value = cmps[2]
+    end
 
-    return {name = name, value = value}
+    return { name = name, value = value }
 end
 
 local function convert_value(value, option_info)
@@ -202,14 +229,15 @@ local function pre_process_set_option(name, value, bufnr)
 
     local option_info = get_option_info(name)
     if option_info.buffer_local == true and bufnr == nil then
-        log.warning("options", "This is only a local option. Use `:Setlocal " ..
-                        name .. "` instead.")
+        log.warning(
+            "options",
+            "This is only a local option. Use `:Setlocal " .. name .. "` instead."
+        )
         return true
     end
 
     if option_info.buffer_local ~= true and bufnr ~= nil then
-        log.warning("options", "This is only a global option. Use `:Set " ..
-                        name .. "` instead.")
+        log.warning("options", "This is only a global option. Use `:Set " .. name .. "` instead.")
         return true
     end
 
@@ -217,15 +245,18 @@ local function pre_process_set_option(name, value, bufnr)
 end
 
 local function set_option(name, value, bufnr)
-    if bufnr == 0 then bufnr = vim.api.nvim_get_current_buf() end
+    if bufnr == 0 then
+        bufnr = vim.api.nvim_get_current_buf()
+    end
 
-    if pre_process_set_option(name, value, bufnr) then return end
+    if pre_process_set_option(name, value, bufnr) then
+        return
+    end
 
     local option_info = get_option_info(name)
     local converted_value = convert_value(value, option_info)
     if converted_value == nil then
-        log.error("options", "Cannot convert `" .. value .. "` to " ..
-                      option_info.type_info .. ".")
+        log.error("options", "Cannot convert `" .. value .. "` to " .. option_info.type_info .. ".")
         return
     end
 
@@ -234,10 +265,10 @@ local function set_option(name, value, bufnr)
         if bufnr ~= nil then
             s_current_options[name] = {
                 value = nil,
-                buffers = {{bufnr = bufnr, value = converted_value}}
+                buffers = { { bufnr = bufnr, value = converted_value } },
             }
         else
-            s_current_options[name] = {value = converted_value, buffers = {}}
+            s_current_options[name] = { value = converted_value, buffers = {} }
         end
 
         current_option = s_current_options[name]
@@ -248,13 +279,16 @@ local function set_option(name, value, bufnr)
     elseif bufnr ~= nil then
         local buffer_option = get_buffer_option(name, bufnr)
         if buffer_option ~= nil then
-            if buffer_option.value == converted_value then return end
+            if buffer_option.value == converted_value then
+                return
+            end
 
             buffer_option.value = converted_value
         elseif buffer_option == nil then
-            table.insert(s_current_options[name].buffers,
-                         {bufnr = bufnr, value = converted_value})
-
+            table.insert(
+                s_current_options[name].buffers,
+                { bufnr = bufnr, value = converted_value }
+            )
         end
 
         execute_callbacks(name)
@@ -272,12 +306,13 @@ function M.get_option_value(name, bufnr)
     end
 
     local buffer_option = nil
-    if bufnr ~= nil then buffer_option = get_buffer_option(name, bufnr) end
+    if bufnr ~= nil then
+        buffer_option = get_buffer_option(name, bufnr)
+    end
 
     if buffer_option ~= nil then
         return buffer_option.value
-    elseif s_current_options[name] ~= nil and s_current_options[name].value ~=
-        nil then
+    elseif s_current_options[name] ~= nil and s_current_options[name].value ~= nil then
         return s_current_options[name].value
     end
 
@@ -290,25 +325,25 @@ function M.register_option(opts)
         return
     end
 
-    assert(not (opts.type_info == nil or opts.type_info == ""),
-           "type_info is a required field.")
+    assert(not (opts.type_info == nil or opts.type_info == ""), "type_info is a required field.")
 
-    assert(type(opts.default) == opts.type_info,
-           "Type of the default value does not match type_info: " ..
-               type(opts.default) .. " != " .. opts.type_info)
+    assert(
+        type(opts.default) == opts.type_info,
+        "Type of the default value does not match type_info: "
+            .. type(opts.default)
+            .. " != "
+            .. opts.type_info
+    )
 
     opts.buffer_local = opts.buffer_local or false
     opts.global = opts.global or not opts.buffer_local
     opts.source = opts.source or ""
 
     if opts.buffer_local and opts.target_variable then
-        assert(false,
-               "You can only use target_variable with global options: " ..
-                   opts.name)
+        assert(false, "You can only use target_variable with global options: " .. opts.name)
     end
 
-    if opts.target_variable ~= nil and
-        not is_target_variable_set(opts.target_variable) then
+    if opts.target_variable ~= nil and not is_target_variable_set(opts.target_variable) then
         set_target_variable(opts.target_variable, opts.default)
     end
 
@@ -319,14 +354,15 @@ function M.register_option(opts)
         buffer_local = opts.buffer_local,
         global = opts.global,
         parser = opts.parser,
-        target_variable = opts.target_variable
+        target_variable = opts.target_variable,
     }
 
-    if opts.target_variable ~= nil and
-        is_target_variable_set(opts.target_variable) then
+    if opts.target_variable ~= nil and is_target_variable_set(opts.target_variable) then
         set_option(opts.name, get_target_variable_value(opts.target_variable))
 
-        if s_variable_sync_initialized == false then init_variable_sync() end
+        if s_variable_sync_initialized == false then
+            init_variable_sync()
+        end
     end
 end
 
@@ -335,7 +371,9 @@ function M.run_set_cmd(option_str, bufnr)
     set_option(opt.name, opt.value, bufnr)
 end
 
-function M.set(name, value) set_option(name, value) end
+function M.set(name, value)
+    set_option(name, value)
+end
 
 function M.set_local(name, value, bufnr)
     if bufnr == nil then
@@ -365,13 +403,12 @@ end
 
 function M.register_callback(name, func)
     if is_option_registered(name) == false then
-        log.error("options",
-                  "Cannot register callback for unregistered option: " .. name)
+        log.error("options", "Cannot register callback for unregistered option: " .. name)
         return
     end
 
     if s_callbacks[name] == nil then
-        s_callbacks[name] = {func}
+        s_callbacks[name] = { func }
     else
         table.insert(s_callbacks[name], func)
     end
@@ -379,12 +416,15 @@ end
 
 function M.set_modeline(bufnr)
     local last_linenr = fn.line("$")
-    if last_linenr == 1 then return end
+    if last_linenr == 1 then
+        return
+    end
 
-    local last_line = vim.api.nvim_buf_get_lines(bufnr, last_linenr - 1,
-                                                 last_linenr, true)[1]
+    local last_line = vim.api.nvim_buf_get_lines(bufnr, last_linenr - 1, last_linenr, true)[1]
 
-    if string.match(last_line, "nvim-options:") == nil then return end
+    if string.match(last_line, "nvim-options:") == nil then
+        return
+    end
 
     local start_index = string.find(last_line, "Setlocal")
     if start_index == nil then
@@ -394,12 +434,16 @@ function M.set_modeline(bufnr)
 
     local modeline = string.sub(last_line, start_index, #last_line)
     modeline = string.split(string.gsub(modeline, "Setlocal ", ""), " ")
-    for _, opt in ipairs(modeline) do M.run_set_cmd(opt, bufnr) end
+    for _, opt in ipairs(modeline) do
+        M.run_set_cmd(opt, bufnr)
+    end
 end
 
 function M.check_for_global_var_change()
     local cmd_line = fn.getreg(":")
-    if string.match(cmd_line, "let g:") == nil then return end
+    if string.match(cmd_line, "let g:") == nil then
+        return
+    end
 
     local var_name = string.match(cmd_line, "g:[^%s.=]+")
     var_name = string.gsub(var_name, "g:", "")
@@ -412,4 +456,3 @@ function M.check_for_global_var_change()
 end
 
 return M
-
